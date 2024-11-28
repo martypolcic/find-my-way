d364180450348d42bf7d96548ffe6fb123a0788c
Martin Polčic <martin.polcic02@gmail.com>
2024-11-28 18:52:42 +0100

    Seeder

--- backend/database/seeders/FlightSeeder.php
+++ backend/database/seeders/FlightSeeder.php
@@ -2,6 +2,8 @@
 
 namespace Database\Seeders;
 
+use App\Models\Airport;
+use App\Models\Flight;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
@@ -12,6 +14,21 @@ class FlightSeeder extends Seeder
      */
     public function run(): void
     {
-        //
+        Airport::factory()
+            ->count(100)
+            ->create();
+
+        // TODO: find a more efficient way to create flights
+        for ($i = 0; $i < 1000; $i++) {
+            $departureAirport = Airport::inRandomOrder()->first();
+            $arrivalAirport = Airport::inRandomOrder()->where('id', '!=', $departureAirport->id)->first();
+
+            Flight::factory()
+                ->hasFlightPrices(3)
+                ->create([
+                    'departure_airport_id' => $departureAirport->id,
+                    'arrival_airport_id' => $arrivalAirport->id,
+                ]);
+        }
     }
 }

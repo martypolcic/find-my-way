4ce10cc0f20c8b389b81dc62725b62db252bed33
Martin Polčic <martin.polcic02@gmail.com>
2024-11-28 13:50:03 +0100

    FlightPrice model

---
+++ backend/app/Http/Controllers/Api/V1/FlightPriceController.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Http\Controllers\Api\V1;
+
+use App\Models\FlightPrice;
+use App\Http\Controllers\Controller;
+
+class FlightPriceController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(FlightPrice $flightPrice)
+    {
+        //
+    }
+}
---
+++ backend/database/seeders/FlightPriceSeeder.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class FlightPriceSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        //
+    }
+}

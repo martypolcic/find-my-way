eef8f35dfba5d5131b57fefb3bb508447caa7d98
Martin Polčic <martin.polcic02@gmail.com>
2024-11-28 13:44:18 +0100

    Airport model

---
+++ backend/app/Http/Controllers/Api/V1/AirportController.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Http\Controllers\Api\V1;
+
+use App\Models\Airport;
+use App\Http\Controllers\Controller;
+
+class AirportController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Airport $airport)
+    {
+        //
+    }
+}

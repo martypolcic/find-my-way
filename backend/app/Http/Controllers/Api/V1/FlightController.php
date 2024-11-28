3401f49b59a4672b7305f2d258f05b4cc570c0cb
Martin Polčic <martin.polcic02@gmail.com>
2024-11-28 13:47:16 +0100

    Flight model

---
+++ backend/app/Http/Controllers/Api/V1/FlightController.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Http\Controllers\Api\V1;
+
+use App\Models\Flight;
+use App\Http\Controllers\Controller;
+
+class FlightController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Flight $flight)
+    {
+        //
+    }
+}

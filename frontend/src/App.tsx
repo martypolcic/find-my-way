dc0666bfbc284e833631c83e2a6e81c3dd83a361
Martin Polčic <martin.polcic02@gmail.com>
2024-12-28 20:55:12 +0100

    Add jumbotron

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,6 +2,7 @@ import { useState } from 'react'
 import './App.css'
 import Header from './components/Header'
 import SearchFlightsForm from './components/SearchFlightsForm'
+import Jumbotron from './components/Jumbotron';
 
 function App() {
   const [activeTab, setActiveTab] = useState('searchFlights');
@@ -14,7 +15,8 @@ function App() {
   return (
     <>
       <Header onChangeTab={handleTabChange}/>
-      {activeTab === 'searchFlights' && <SearchFlightsForm />}
+      <Jumbotron/>
+      <SearchFlightsForm />
     </>
   )
 }
---
+++ frontend/src/components/Jumbotron.tsx
@@ -0,0 +1,18 @@
+import './Jumbotron.css'
+
+function Jumbotron() {
+  return (
+    <div className='jumbotron'>
+        <h1>Explore</h1>
+        <div className='jumbotron-content'>
+            <div className='jumbotron-text'>
+                <h2>Discover the best deals on flights for your next vacation or weekend getaway with Find My Way.</h2>
+                <button>Start your adventure</button>
+            </div>
+            <div className='jumbotron-img'></div>
+        </div>
+    </div>
+  );
+}
+
+export default Jumbotron;

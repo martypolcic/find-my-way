7f2f9109fce49a03d1fb56590f1e32dc069ec4fb
DrWarpMan <drwarpman@gmail.com>
2025-01-09 18:30:06 +0100

    add swagger

--- backend/app/Http/Controllers/Controller.php
+++ backend/app/Http/Controllers/Controller.php
@@ -2,7 +2,11 @@
 
 namespace App\Http\Controllers;
 
-abstract class Controller
+use Illuminate\Routing\Controller as BaseController;
+use OpenApi\Attributes as OA;
+
+#[OA\Info(version: '0.1.0', title: 'Find My Way API Documentation')]
+abstract class Controller extends BaseController
 {
-    //
+    
 }

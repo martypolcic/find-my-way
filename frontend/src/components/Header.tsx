31ed7cb3e9a3e703c912a755fc1b2d84a65c17b7
Martin Polčic <martin.polcic02@gmail.com>
2025-03-16 23:54:50 +0100

    Update header

--- frontend/src/components/Header.tsx
+++ frontend/src/components/Header.tsx
@@ -1,13 +1,14 @@
 import './Header.css'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faEarthEurope } from '@fortawesome/free-solid-svg-icons'
 
-interface HeaderProps {
-  onChangeTab: (tabName: string) => void;
-}
-
-function Header({ onChangeTab }: HeaderProps) {
+function Header() {
   return (
     <header>
-        <button onClick={() => onChangeTab('searchFlights')}>Home</button>
+      <span className='flex gap-2 items-center justify-center text-xl p-4'>
+        <FontAwesomeIcon icon={faEarthEurope} />
+        <h1>Find my way</h1>
+      </span>
     </header>
   );
 }
